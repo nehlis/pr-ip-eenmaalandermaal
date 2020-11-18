04085d0c12dca51c5c9a7a438fc6eb0848e761d3
Thijmen Maus <thijmen.maus@mundaro.nl>
2020-11-18 15:53:00 +0100

    quick fix

--- components/card.php
+++ components/card.php
@@ -4,7 +4,7 @@
         <h5 class="card-title text-uppercase mb-5"><?= $title ?? '<< geen titel meegegeven aan deze kaart >>'; ?></h5>
         <div class="row">
             <div class="col-xs-12 col-xl-6">
-                <h2 class="font-weight-bold mb-0">€ <?= $prijs ?? '42,50'; ?></h2>
+                <h2 class="font-weight-bold mb-0">€ <?= $prijs ?? '0,-'; ?></h2>
             </div>
             <div class="col-xs-12 col-xl-6">
                 <a href="<?= $destination ?? '#'; ?>" class="btn btn-primary btn-block text-uppercase">bekijken</a>
