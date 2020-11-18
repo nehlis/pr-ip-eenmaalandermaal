483fceb45e2a21d413e930157746b1787edd0ff0
Niels Bosman <niels.bosman@student.graafschapcollege.nl>
2020-11-18 10:09:14 +0100

    SCSS support

--- helpers/constants.php
+++ helpers/constants.php
@@ -2,7 +2,7 @@
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT_DIR', realpath(__DIR__ . DS . '..'));
-define('ASSETS_DIR', DS . 'public'. DS . 'assets');
+define('ASSETS_DIR', DS . 'public' . DS . 'assets');
 define('VIEWS_DIR', ROOT_DIR . DS . 'views');
 define('COMPONENTS_DIR', ROOT_DIR . DS . 'components');
 define('LAYOUTS_DIR', ROOT_DIR . DS . 'layouts');
