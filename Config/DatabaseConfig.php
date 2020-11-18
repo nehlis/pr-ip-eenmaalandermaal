f143d2eafbf82f5962b0464ab785fac6cc2cbfac
ilivss <ilivss@pm.me>
2020-11-18 19:26:38 +0100

    Update DatabaseConfig.php

--- Config/DatabaseConfig.php
+++ Config/DatabaseConfig.php
@@ -2,9 +2,11 @@
 
 namespace Config;
 
-class DatabaseConfig {
+class DatabaseConfig
+{
     public const HOST = 'mssql.iproject.icasites.nl';
     public const DATABASE = 'iproject3';
     public const USER = 'iproject3';
     public const PASSWORD = 'aeedZ8u8';
 }
+`
