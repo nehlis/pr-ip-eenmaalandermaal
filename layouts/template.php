3bfc2677101c058ed86292c2add531a3aa1a940a
Niels Bosman <niels.bosman@student.graafschapcollege.nl>
2020-11-18 10:58:11 +0100

    Refactor Component render

--- layouts/template.php
+++ layouts/template.php
@@ -1,3 +1,4 @@
+<?php use Core\Component; ?>
 <!doctype html>
 <html lang="en">
 
@@ -11,12 +12,15 @@
 </head>
 
 <body>
-    <?php include_once COMPONENTS_DIR . DS . 'navbar.php' ?>
+    <?php
+    
+    Component::render('navbar');
 
-    <?php include_once VIEWS_DIR . DS . $view . '.php'; ?>
+    include_once VIEWS_DIR . DS . $view . '.php';
 
-    <?php include_once COMPONENTS_DIR . DS . 'footer.php' ?>
-
+    Component::render('footer');
+    
+    ?>
     <script src="<?= ASSETS_DIR; ?>/js/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
