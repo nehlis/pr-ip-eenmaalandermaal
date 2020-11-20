5bf231264557e3913d1fa350a890849854aa03af
nehlis <nielsbosman@mail.com>
2020-11-20 21:29:15 +0100

    Cleanup v4 (naming)

--- Core/Database.php
+++ Core/Database.php
@@ -19,14 +19,14 @@ use Error;
 class Database
 {
     /**
-     * @var PDO $dbh PDO Handler also known as the database handler aka the database connection.
+     * @var PDO $pdo PDO Handler also known as the database handler aka the database connection.
      */
-    private $dbh;
+    private $pdo;
 
     /**
-     * @var PDOStatement $sth Statement handler.
+     * @var PDOStatement $statement Statement handler.
      */
-    private $sth;
+    private $statement;
 
     /**
      * Connect to the database.
@@ -35,14 +35,14 @@ class Database
     private function connect(): void
     {
         try {
-            $this->dbh = new PDO(
+            $this->pdo = new PDO(
                 "sqlsrv:server=" . DatabaseConfig::HOST . "; Database=" . DatabaseConfig::DATABASE,
                 DatabaseConfig::USER,
                 DatabaseConfig::PASSWORD
             );
 
             // Uncomment bottom line when debugging
-            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $ex) {
             $this->handlePDOException($ex->getMessage());
         }
@@ -54,7 +54,7 @@ class Database
      */
     private function close(): void
     {
-        unset($this->sth, $this->dbh);
+        unset($this->statement, $this->pdo);
     }
 
     /**
@@ -62,7 +62,6 @@ class Database
      * @param  string $table Table to insert data in.
      * @param  array  $data  Associative array of which the key represents the column name.
      * @return void
-     * @throws Error         Throws error when execution fails. Possible cause: SQL conflicts
      */
     public function create(string $table, array $data): void
     {
@@ -71,7 +70,7 @@ class Database
         $columns = implode(', ', array_keys($data));
         $values  = $this->formatInsertValues(array_values($data));
         
-        $this->sth = $this->dbh->prepare("INSERT INTO $table ($columns) VALUES ($values)");
+        $this->statement = $this->pdo->prepare("INSERT INTO $table ($columns) VALUES ($values)");
 
         $this->execute();
         $this->close();
@@ -82,13 +81,12 @@ class Database
      * @param  string $table Table to fetch data from.
      * @param  int    $id    Row with ID.
      * @return array         Returns fetched row as an associative array.
-     * @throws Error         Throws error when nothing was found or when execution fails (SQL related).
      */
     public function get(string $table, int $id): ?array
     {
         $this->connect();
 
-        $this->sth = $this->dbh
+        $this->statement = $this->pdo
             ->prepare("SELECT * FROM $table where ID = :id")
             ->bindParam(':id', $id, PDO::PARAM_INT);
 
@@ -107,13 +105,12 @@ class Database
      * @param  string $column Row where column = 'value'.
      * @param  string $value  Value to check for in table.
      * @return array          Returns fetched row as an associative Arrays.
-     * @throws Error          Throws error when nothing was found or when execution fails (SQL related).
      */
     public function getByColumn(string $table, string $column, string $value): ?array
     {
         $this->connect();
         
-        $this->sth = $this->dbh
+        $this->statement = $this->pdo
             ->prepare("SELECT * FROM $table where $column = :value")
             ->bindParam(':value', $value, PDO::PARAM_STR);
 
@@ -130,13 +127,12 @@ class Database
      * Read method to be used in Controllers.
      * @param  string $table Table to fetch data from.
      * @return array         Returns numeric array with all rows (as an associative arrays).
-     * @throws Error         Throws error when nothing was found or when execution fails (SQL related).
      */
     public function index(string $table): ?array
     {
         $this->connect();
         
-        $this->sth = $this->dbh->prepare("SELECT * FROM $table");
+        $this->statement = $this->pdo->prepare("SELECT * FROM $table");
 
         $result = $this
             ->execute()
@@ -157,7 +153,7 @@ class Database
     {
         $this->connect();
         
-        $this->sth = $this->dbh
+        $this->statement = $this->pdo
             ->prepare("UPDATE $table SET " . $this->formatUpdateValues($data) . " WHERE ID = :id")
             ->bindParam(':id', $id, PDO::PARAM_INT);
 
@@ -170,13 +166,12 @@ class Database
      * @param  string $table Table to delete a row
      * @param  int    $id    Delete row where ID = ?
      * @return void          Returns nothing.
-     * @throws Error         Throws error when execution fails. Possible cause: SQL conflicts
      */
     public function delete(string $table, int $id): void
     {
         $this->connect();
         
-        $this->sth = $this->dbh
+        $this->statement = $this->pdo
             ->prepare("DELETE FROM $table WHERE ID = :id")
             ->bindParam(':id', $id, PDO::PARAM_INT);
     
@@ -230,11 +225,11 @@ class Database
     private function execute(): PDOStatement
     {
         try {
-            $this->sth->execute();
+            $this->statement->execute();
         } catch (PDOException $ex) {
             $this->handlePDOException($ex->getMessage());
         }
         
-        return $this->sth;
+        return $this->statement;
     }
 }
