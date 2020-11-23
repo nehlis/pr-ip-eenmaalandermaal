4b4508662277092edf7f8c17378c634385f9caed
nehlis <nielsbosman@mail.com>
2020-11-23 14:24:31 +0100

    Add PhpDoc

--- App/Controllers/AccountController.php
+++ App/Controllers/AccountController.php
@@ -22,29 +22,47 @@ class AccountController implements IController
      */
     private static $table = 'Account';
     
-
+    /**
+     * AccountController constructor.
+     */
     public function __construct()
     {
         $this->database = new Database;
     }
     
+    /**
+     * @param array $data
+     * @return array|null
+     */
     public function create(array $data): ?array
     {
         $this->database->create(self::$table, $data);
         
         return $this->database->getByColumn(self::$table, 'email', $data['email']);
     }
-
+    
+    /**
+     * @param int $id
+     * @return array|null
+     */
     public function get(int $id): ?array
     {
         return $this->database->get(self::$table, $id);
     }
-
+    
+    /**
+     * @return array|null
+     */
     public function index(): ?array
     {
         return $this->database->index(self::$table);
     }
     
+    /**
+     * @param int   $id
+     * @param array $data
+     * @return array|null
+     */
     public function update(int $id, array $data): ?array
     {
         if (!$this->get($id)) return null;
@@ -53,7 +71,11 @@ class AccountController implements IController
         
         return $this->get($id);
     }
-
+    
+    /**
+     * @param int $id
+     * @return array|null
+     */
     public function delete(int $id): ?array
     {
         if (!$user = $this->get($id)) return null;
