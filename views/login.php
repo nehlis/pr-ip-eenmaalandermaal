a43fdb3a7f5cb96b947500e0f7715eedd86d0b4b
Simon Rijnbende <simon.rijnbende@mundaro.nl>
2020-11-24 10:52:22 +0100

    Getting the email and password

--- views/login.php
+++ views/login.php
@@ -1,21 +1,27 @@
-<div class="signin-wrapper py-5">
-  <form class="form-signin py-5">
-    <h1 class="h3 mb-3 font-weight-normal text-center">Log in met uw account</h1>
-    <label for="inputEmail" class="sr-only">Emailadres</label>
-    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-    <label for="inputPassword" class="sr-only">Wachtwoord</label>
-    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-    <div class="row">
-      <div class="checkbox col">
-        <label>
-          <input type="checkbox" value="remember-me"> Onthoud mij
-        </label>
+<?php if (!empty($_POST)) : ?>
+  Email: <?php echo htmlspecialchars($_POST["email"]); ?>!<br>
+  Password: <?php echo htmlspecialchars($_POST["password"]); ?>.<br>
+<?php else : ?>
+  <div class="signin-wrapper py-5">
+    <form action="/inloggen" method="POST" class="form-signin py-5">
+      <h1 class="h3 mb-3 font-weight-normal text-center">Log in met uw account</h1>
+      <label for="inputEmail" class="sr-only">Emailadres</label>
+      <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only">Wachtwoord</label>
+      <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+      <div class="row">
+        <div class="checkbox col">
+          <label>
+            <input type="checkbox" value="remember-me"> Onthoud mij
+          </label>
+        </div>
+        <div class="mb-3 col">
+          <a href="/registreren">Nog geen account?</a>
+        </div>
       </div>
-      <div class="mb-3 col">
-        <a href="/registreren">Nog geen account?</a>
-      </div>
-    </div>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Inloggen</button>
-    <p class="mt-5 mb-3 text-muted">&copy; <?= date('Y'); ?></p>
-  </form>
-</div>
+      
+      <input class="btn btn-lg btn-primary btn-block" type="submit" value="Inloggen" name='submit'/>
+      <p class="mt-5 mb-3 text-muted">&copy; <?= date('Y'); ?></p>
+    </form>
+  </div>
+<?php endif; ?>
