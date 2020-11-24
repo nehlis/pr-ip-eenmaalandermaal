6d6cdb4170b0f48397370e9735e9f2ab42cd2d02
nehlis <nielsbosman@mail.com>
2020-11-24 16:05:50 +0100

    PhpDoc

--- App/Controllers/CountryController.php
+++ App/Controllers/CountryController.php
@@ -6,6 +6,10 @@ use App\Core\Database;
 use App\Interfaces\IController;
 use Error;
 
+/**
+ * Class CountryController
+ * @package App\Controllers
+ */
 class CountryController implements IController
 {
     /**
@@ -26,26 +30,46 @@ class CountryController implements IController
         $this->database = new Database;
     }
     
+    /**
+     * @param array $data
+     * @return array|null
+     */
     public function create(array $data): ?array
     {
         // TODO: Implement create() method.
     }
     
+    /**
+     * @param int $id
+     * @return array|null
+     */
     public function get(int $id): ?array
     {
         // TODO: Implement get() method.
     }
     
+    /**
+     * @return array|null
+     */
     public function index(): ?array
     {
         return $this->database->index(self::$table);
     }
     
+    /**
+     * @param int   $id
+     * @param array $data
+     * @return array|null
+     */
     public function update(int $id, array $data): ?array
     {
         // TODO: Implement update() method.
     }
     
+    /**
+     * @param int $id
+     * @return array|null
+     */
     public function delete(int $id): ?array
     {
         // TODO: Implement delete() method.
--- App/Controllers/QuestionController.php
+++ App/Controllers/QuestionController.php
@@ -6,6 +6,10 @@ use App\Core\Database;
 use App\Interfaces\IController;
 use Error;
 
+/**
+ * Class QuestionController
+ * @package App\Controllers
+ */
 class QuestionController implements IController
 {
     /**
@@ -26,26 +30,46 @@ class QuestionController implements IController
         $this->database = new Database;
     }
     
+    /**
+     * @param array $data
+     * @return array|null
+     */
     public function create(array $data): ?array
     {
         // TODO: Implement create() method.
     }
     
+    /**
+     * @param int $id
+     * @return array|null
+     */
     public function get(int $id): ?array
     {
         // TODO: Implement get() method.
     }
     
+    /**
+     * @return array|null
+     */
     public function index(): ?array
     {
         return $this->database->index(self::$table);
     }
     
+    /**
+     * @param int   $id
+     * @param array $data
+     * @return array|null
+     */
     public function update(int $id, array $data): ?array
     {
         // TODO: Implement update() method.
     }
     
+    /**
+     * @param int $id
+     * @return array|null
+     */
     public function delete(int $id): ?array
     {
         // TODO: Implement delete() method.
