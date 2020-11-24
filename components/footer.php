b18c2cdff497451ad92f30b0e7ba9aa2efbd910a
Simon Rijnbende <simon.rijnbende@mundaro.nl>
2020-11-24 16:28:58 +0100

    Footer

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,61 @@
 <footer class="footer">
-    <p>Dummy Text <a href="#">Link</a>.</p>
-    <p>
-        <a href="#">Back to top</a>
-    </p>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md">
+                <a class="navbar-brand mb-0 h1" href="#">
+                    EenmaalAndermaal
+                </a>
+                <small class="d-block mb-3 text-muted">&copy; <?= date('Y'); ?></small>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Contact</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Contactgegevens</a></li>
+                    <li><a class="text-muted" href="#">Veelgestelde vragen</a></li>
+                    <li><a class="text-muted" href="#">Privacy</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Over ons</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Hoe werkt het?</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Jouw profiel</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="/registreren">Profiel aanmaken</a></li>
+                    <li><a class="text-muted" href="#">Mijn profiel</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<footer class="bg-primary text-white copyright">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <span>Copyright &copy; <?= date('Y'); ?>, All Right Reserved EenmaalAndermaal</span>
+            </div>
+            <div class="col-md-6">
+                <div class="copyright-menu">
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Algemene voorwaarden</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 </footer>
