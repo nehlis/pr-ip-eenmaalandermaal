4615a9b64b4e1d35ef4e69ad4cea7e639bf49327
Thijmen Maus <thijmen.maus@mundaro.nl>
2020-12-08 14:35:15 +0100

    responsive + images

--- views/auction.php
+++ views/auction.php
@@ -1,35 +1,42 @@
 <?php
 
 use App\Controllers\BiddingController;
+use App\Controllers\FileController;
 use App\Core\Component;
 use App\Controllers\ItemController;
 use App\Services\AuthService;
 
 $ic = new ItemController;
 $bc = new BiddingController;
+$fc = new FileController;
 
 $isLoggedIn = AuthService::isLoggedIn();
 
-$veilingId = (int)$_GET['id'];
-$veilingDetails = $ic->getDetailed($veilingId);
-$veiling = $veilingDetails[0];
+$auctionId = (int)$_GET['id'];
+$auction = $ic->getDetailed($auctionId)[0];
 
-$biddings = $bc->indexBiddingsWithUsernameByAuctionId($veilingId);
+$biddings = $bc->indexBiddingsWithUsernameByAuctionId($auctionId);
 
-$veilingImages = array(PLACEHOLDER, PLACEHOLDER_ALT, PLACEHOLDER);
+$veilingImages = array(PLACEHOLDER);
+
+try {
+    $veilingImages = $fc->getByAuctionId($auctionId);
+} catch (Exception $ex) {
+    $veilingImages = array(PLACEHOLDER);
+}
 
 if (isset($_POST) && count($_POST) > 0) {
     if (isset($_SESSION['id'])) {
         $userId = $_SESSION['id'];
         $bidding = $_POST['bidding'];
-        if ($bidding > $veiling['BiddingAmount']) {
+        if ($bidding > $auction['BiddingAmount']) {
             $bc->create([
-                'ItemID' => $veilingId,
+                'ItemID' => $auctionId,
                 'AccountID' => $userId,
                 'Time' => date("Y-m-d H:i:s"),
                 'Amount' => $bidding
             ]);
-            echo "<script>window.location.href = '/veiling?id=$veilingId';</script>";
+            echo "<script>window.location.href = '/veiling?id=$auctionId';</script>";
         }
     }
 }
@@ -40,18 +47,18 @@ if (isset($_POST) && count($_POST) > 0) {
 <main role="main" class="container">
     <div class="row my-5">
         <div class="col-12 col-lg-8 d-flex align-items-end">
-            <h3 class="font-weight-bold text-dark"><?= $veiling['Title'] ?></h3>
+            <h3 class="font-weight-bold text-dark"><?= $auction['Title'] ?></h3>
         </div>
         <div class="col-12 col-lg-4 mb-4">
             <div class="row d-flex justify-content-end">
                 <div class="col-12">
                     <h4 class="text-secondary">
                         Totale looptijd: <script>
-                            getTotalDuration("<?= $veiling['StartDate'] ?>", "<?= $veiling['EndDate'] ?>");
+                            getTotalDuration("<?= $auction['StartDate'] ?>", "<?= $auction['EndDate'] ?>");
                         </script>
                     </h4>
                     <div class="bg-primary p-3 rounded text-center">
-                        <h1 class="text-white m-0 p-0 font-weight-bold countdownTimer" id="<?= $veiling['EndDate'] ?>">
+                        <h1 class="text-white m-0 p-0 font-weight-bold countdownTimer" id="<?= $auction['EndDate'] ?>">
                             Laden..
                         </h1>
                     </div>
@@ -60,12 +67,12 @@ if (isset($_POST) && count($_POST) > 0) {
         </div>
         <div class="col-12 col-lg-8">
             <?php Component::render("carousel", [
-                'images' => $veilingImages
+                'images' => array_column($veilingImages, 'FileName')
             ]); ?>
         </div>
         <div class="col-12 col-lg-4">
             <small class="text-secondary font-weight-bold">Omschrijving</small>
-            <p><?= $veiling['Description'] ?></p>
+            <p><?= $auction['Description'] ?></p>
         </div>
         <div class="col-12 my-3">
             <div class="dropdown-divider"></div>
@@ -76,22 +83,22 @@ if (isset($_POST) && count($_POST) > 0) {
                     <div class="row">
                         <div class="col-12 col-md-6">
                             <h2 class="text-primary font-weight-bold">Huidig bod</h2>
-                            <p class="display-3 text-dark font-weight-bold">&euro; <?= $veiling['BiddingAmount'] ?? '0,-' ?></p>
+                            <p class="display-3 text-dark font-weight-bold">&euro; <?= $auction['BiddingAmount'] ?? '0,-' ?></p>
                         </div>
                         <div class="col-12 my-3 d-block d-md-none">
                             <div class="dropdown-divider w-100"></div>
                         </div>
                         <div class="col-12 col-md-6">
                             <h2 class="text-primary font-weight-bold">Nieuw bod</h2>
-                            <small class="font-weight-bold d-block mb-2">Minimum bod: &euro; <?= $veiling['BiddingAmount'] ? $veiling['BiddingAmount'] + 0.1 : $veiling['StartingPrice'] ?></small>
+                            <small class="font-weight-bold d-block mb-2">Minimum bod: &euro; <?= $auction['BiddingAmount'] ? $auction['BiddingAmount'] + 0.1 : $auction['StartingPrice'] ?></small>
                             <div class="col-12 p-0">
-                                <form class="row" action="/veiling?id=<?= $veilingId ?>" method="POST">
+                                <form class="row" action="/veiling?id=<?= $auctionId ?>" method="POST">
                                     <div class="col-12 col-sm-6 mb-2">
                                         <div class="input-group">
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text">&euro;</span>
                                             </div>
-                                            <input type="number" id='biddingInput' min="<?= $biddings ? $veiling['BiddingAmount'] + 0.1 : $veiling['StartingPrice']  ?>" step="0.1" class="form-control" placeholder="<?= $biddings ? $veiling['BiddingAmount'] + 0.1 : $veiling['StartingPrice'] ?>" aria-label="Bidding amount" aria-describedby="basic-addon2" <?php if (!$isLoggedIn) : ?> disabled <?php endif ?> name="bidding">
+                                            <input type="number" id='biddingInput' min="<?= $biddings ? $auction['BiddingAmount'] + 0.1 : $auction['StartingPrice']  ?>" step="0.1" class="form-control" placeholder="<?= $biddings ? $auction['BiddingAmount'] + 0.1 : $auction['StartingPrice'] ?>" aria-label="Bidding amount" aria-describedby="basic-addon2" <?php if (!$isLoggedIn) : ?> disabled <?php endif ?> name="bidding">
                                         </div>
                                     </div>
                                     <div class="col-12 col-sm-6 pl-sm-0 mb-3 mb-sm-0">
@@ -124,7 +131,9 @@ if (isset($_POST) && count($_POST) > 0) {
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php $i = 0; ?>
                                 <?php foreach ($biddings as $bidding) : ?>
+                                    <?php if (++$i > 3) break ?>
                                     <tr>
                                         <th scope="row"><?= $bidding['Username'] ?></th>
                                         <td><?= date("H:i:s", strtotime($bidding['Time'])) ?></td>
@@ -149,7 +158,7 @@ if (isset($_POST) && count($_POST) > 0) {
                 <div class="col-12 col-md-4">
                     <h2 class="font-weight-bold text-primary">Betaling</h2>
                     <small class="text-secondary font-weight-bold">Betalingsinstructies</small>
-                    <p><?= $veiling['PaymentInstructions'] ?? "De verkoper heeft geen betalingsinstructies opgegeven bij deze veiling.." ?></p>
+                    <p><?= $auction['PaymentInstructions'] ?? "De verkoper heeft geen betalingsinstructies opgegeven bij deze veiling.." ?></p>
                 </div>
                 <div class="col-12 my-3 d-block d-md-none">
                     <div class="dropdown-divider w-100"></div>
@@ -157,7 +166,7 @@ if (isset($_POST) && count($_POST) > 0) {
                 <div class="col-12 col-md-4">
                     <h2 class="font-weight-bold text-primary">Verzending</h2>
                     <small class="text-secondary font-weight-bold">Verzendinstructies</small>
-                    <p><?= $veiling['SendInstructions'] ?? "De verkoper heeft geen verzendinformatie opgegeven bij deze veiling.." ?></p>
+                    <p><?= $auction['SendInstructions'] ?? "De verkoper heeft geen verzendinformatie opgegeven bij deze veiling.." ?></p>
                 </div>
                 <div class="col-12 my-3 d-block d-md-none">
                     <div class="dropdown-divider w-100"></div>
@@ -165,9 +174,9 @@ if (isset($_POST) && count($_POST) > 0) {
                 <div class="col-12 col-md-4">
                     <h2 class="font-weight-bold text-primary">Verkoper</h2>
                     <small class="text-secondary font-weight-bold">Gebruikersnaam</small>
-                    <p><?= $veiling['Username'] ?></p>
+                    <p><?= $auction['Username'] ?></p>
                     <small class="text-secondary font-weight-bold">Locatie</small>
-                    <p><?= $veiling['City'] ?></p>
+                    <p><?= $auction['City'] ?></p>
                 </div>
             </div>
         </div>
